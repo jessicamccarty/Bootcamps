20941ef2fa5dae774ade11f8718a10e0251cdc6a
jessicamccarty <jessicaeitr@gmail.com>
2024-11-02 13:19:25 -0500

    CSharpCalebCurry

---
+++ CSharpCalebCurry/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpCalebCurry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); // Statement that tells the computer what to do.
+        }
+    }
+}
